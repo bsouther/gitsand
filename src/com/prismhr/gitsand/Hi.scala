48a949c3bc9553417edd48c2bf214d850f595e6a
Ben Souther <bpsouther@localhost.localdomain>
2016-12-19 20:25:43 -0500

    Fixed punctuation
    Added comment.

--- src/com/prismhr/gitsand/Hi.scala
+++ src/com/prismhr/gitsand/Hi.scala
@@ -1,7 +1,17 @@
 package com.prismhr.gitsand
 
 object Hi{
+  /**
+   * Fancy hello, world program.
+   */
   def main(args:Array[String]){
-    println("Hello");
+    val recip:String = {
+      if(args.size < 1 || "".equals(args(0).trim())){
+        "World"
+      }else{
+        args(0)
+      }
+    }
+    println("Hello, " + recip)
   }
 }
