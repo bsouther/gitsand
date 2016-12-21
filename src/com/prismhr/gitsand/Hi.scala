2248df7c822af9cab81e2f8e2cb6b05c6064672c
Ben Souther <bpsouther@localhost.localdomain>
2016-12-20 20:27:28 -0500

    Scala version now uses fold
    Shortening.
    
     fold + foreach

--- src/com/prismhr/gitsand/Hi.scala
+++ src/com/prismhr/gitsand/Hi.scala
@@ -1,21 +1,9 @@
 package com.prismhr.gitsand
 
-object Hi{
-
-  /**
-   * Fancy hello, world program.
-   */
-  def main(args:Array[String]){
-
-
-    val recip:Array[String] = {
-      if(args.size < 1 || "".equals(args(0).trim())){
-        Array[String]("World")
-      }else{
-        args
-      }
-    }
-
-    recip.foreach( (name) => { println( "Hello, " + name + ".") }  );
+object Hi {
+  def main(args:Array[String]):Unit = {
+    args.map(a => {
+      "Hola, " + a
+    }).foreach(println);
   }
 }
