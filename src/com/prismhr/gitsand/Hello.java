6bfc7327ff2e7cf5dc50d66541e2f27376d40e42
Ben Souther <bpsouther@localhost.localdomain>
2016-12-21 22:33:30 -0500

    Added javadoc comment

--- src/com/prismhr/gitsand/Hello.java
+++ src/com/prismhr/gitsand/Hello.java
@@ -1,6 +1,10 @@
 package com.prismhr.gitsand;
 
 public class Hello{
+
+    /**
+     * Entry Point
+     */
     public static void main(String ... args){
         System.out.println("Hello, World (Java Edition)");
     }
