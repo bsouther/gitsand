c7444efd8b1586f332951dfd3077e2832df4116a
Ben Souther <bpsouther@localhost.localdomain>
2016-12-21 22:26:07 -0500

    Added logging (Actually, just a print statement)

--- src/com/prismhr/gitsand/Hi.scala
+++ src/com/prismhr/gitsand/Hi.scala
@@ -1,6 +1,13 @@
 package com.prismhr.gitsand
 
+
 object Hi {
+
+
+  println("initialzing")
+
+
+
   def main(args:Array[String]):Unit = {
     args.foreach(a => println("Hola, " + a + ". (Scala Edition)") )
   }
